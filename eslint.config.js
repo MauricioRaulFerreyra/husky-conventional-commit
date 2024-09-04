3d259c68eb3d0d85e52e2a1a4cbffb03ba767a2b
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2024-09-04 12:58:19 -0300

    primer commit de prueba - first test commit

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+//@ts-ignore
+import js from "@eslint/js";
+import globals from "globals";
+//@ts-ignore
+import reactHooks from "eslint-plugin-react-hooks";
+//@ts-ignore
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist", "package.json", "node_modules"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier.format,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "eslint-plugin-prettier": eslintPluginPrettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-duplicate-enum-values": "warn",
+      "no-empty-function": "error",
+      "no-implicit-coercion": "warn",
+    },
+  },
+);
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  const A = 0;
+  if (A == count) {
+    console.log("Hola");
+  }
+
+  return (
+    <>
+      <div className="bg-gray-400 sm:bg-slate-600 lg:bg-blue-600 xl:bg-red-500">
+        <h1>Hello Vite + React!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
